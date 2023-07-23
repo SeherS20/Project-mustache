1ca6afd9a63613f55ce1bb22d397bfdd6026f5b8
SeherS20 <99300690+SeherS20@users.noreply.github.com>
2023-07-22 20:45:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ function preload(){
 function setup(){
     canvas = createCanvas(300,300)
     canvas.center();
+    video = createCapture(VIDEO)
+    video.size(300,200)
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+function modelLoaded(){
+    console.log("Posenet has loaded")
+
 }
 function draw(){
-
+    image(video, -75, 0, 450, 300)
 }
 function snapshot(){
     save('mustache.png')
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        console.log("nose x -" + results[0].pose.nose.y)
+        console.log("nose y -" + results[0].pose.nose.y)
+    }
 }
